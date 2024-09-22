d7159d1f1dc30cba8aacc1f8d6efe6e768835569
ledoyen <ledoyen.loic@gmail.com>
2024-09-22 17:51:30 +0200

    Upgrade KTK & JDK

--- src/main/java/com/github/lernejo/korekto/grader/api/JavaApiGrader.java
+++ src/main/java/com/github/lernejo/korekto/grader/api/JavaApiGrader.java
@@ -1,22 +1,16 @@
 package com.github.lernejo.korekto.grader.api;
 
 import com.github.lernejo.korekto.grader.api.parts.*;
-import com.github.lernejo.korekto.toolkit.*;
+import com.github.lernejo.korekto.toolkit.Grader;
+import com.github.lernejo.korekto.toolkit.GradingConfiguration;
 import com.github.lernejo.korekto.toolkit.PartGrader;
-import com.github.lernejo.korekto.toolkit.misc.HumanReadableDuration;
 import com.github.lernejo.korekto.toolkit.partgrader.*;
-import com.github.lernejo.korekto.toolkit.thirdparty.git.GitContext;
-import com.github.lernejo.korekto.toolkit.thirdparty.git.GitNature;
 import com.github.lernejo.korekto.toolkit.thirdparty.pmd.Rule;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.Instant;
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class JavaApiGrader implements Grader<LaunchingContext> {
 
@@ -32,27 +26,7 @@ public class JavaApiGrader implements Grader<LaunchingContext> {
         return new LaunchingContext(configuration);
     }
 
-    @Override
-    public void run(LaunchingContext context) {
-        context.getGradeDetails().getParts().addAll(grade(context));
-    }
-
-    private Collection<? extends GradePart> grade(LaunchingContext context) {
-        return graders().stream()
-            .map(g -> applyPartGrader(context, g))
-            .collect(Collectors.toList());
-    }
-
-    private GradePart applyPartGrader(LaunchingContext context, PartGrader<LaunchingContext> g) {
-        long startTime = System.currentTimeMillis();
-        try {
-            return g.grade(context);
-        } finally {
-            logger.debug(g.name() + " in " + HumanReadableDuration.toString(System.currentTimeMillis() - startTime));
-        }
-    }
-
-    private Collection<? extends PartGrader<LaunchingContext>> graders() {
+    public Collection<PartGrader<LaunchingContext>> graders() {
         return List.of(
             new MavenCompileAndTestPartGrader<>(
                 "Part 1 - Compilation & Tests",
@@ -66,7 +40,7 @@ public class JavaApiGrader implements Grader<LaunchingContext> {
             new Part8Grader("Part 8 - Fire API (server)", 2.0D),
             new Part9Grader("Part 9 - Game self convergence", 5.0D),
             new GitHistoryPartGrader<>("Git (proper descriptive messages)", -4.0D),
-            new PmdPartGrader<>("Coding style", -20.0D,-1.0D,
+            new PmdPartGrader<>("Coding style", -20.0D, -1.0D,
                 Rule.buildExcessiveClassLengthRule(90),
                 Rule.buildExcessiveMethodLengthRule(15),
                 Rule.buildFieldMandatoryModifierRule(0, "private", "final", "!static"),
@@ -83,7 +57,7 @@ public class JavaApiGrader implements Grader<LaunchingContext> {
 
     @Override
     public String slugToRepoUrl(String slug) {
-        return "https://github.com/" + slug + "/java_api_training";
+        return "https://github.com/" + slug + "/navy_battle_project";
     }
 
     @Override
--- src/test/java/com/github/lernejo/korekto/grader/api/JavaApiGraderTest.java
+++ src/test/java/com/github/lernejo/korekto/grader/api/JavaApiGraderTest.java
@@ -1,17 +1,6 @@
 package com.github.lernejo.korekto.grader.api;
 
-import com.github.lernejo.korekto.toolkit.*;
 import com.github.lernejo.korekto.toolkit.tests.EndToEndGraderTest;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class JavaApiGraderTest extends EndToEndGraderTest {
-
 }
